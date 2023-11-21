84146a5121ff69069ffaf83f45e8cb02191c126a
Soulra01 <raden.raharja@binus.ac.id>
2023-11-21 18:36:54 +0700

    cek

--- component/login.php
+++ component/login.php
@@ -3,16 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wear House - Login</title>
-    <link rel="stylesheet" href="../assets/global.css">
-    <link rel="stylesheet" href="../assets/login/login.css">
+    <title>Document</title>
     <link rel="stylesheet" href="../assets/navbar.css">
-    <link rel="stylesheet" href="../assets/footer.css">
-    <script src="../script/jquery.js"></script>
-    <script src="../script/login.js"></script>
+    <!-- <link rel="stylesheet" href="../assets/global.css"> -->
 </head>
 <body>
-    <!-- Navbar -->
+
+    <!-- NAVBAR SUDAH RESPONSIVE -->
     <nav class="navbar">
         <div class="kiri">
             <ul>
@@ -30,7 +27,7 @@
                 <li><label for="tri" id="segitiga">▼</label></li>
                 <button id="LogOut" onclick="doLogOut()">LogOut</button>
                 <div id="loginRegister">
-                    <li><a href="./login.html">Login</a></li>
+                    <li><a href="./login.php">Login</a></li>
                     <li><a href="./register.html">Register</a></li>
                 </div>
             </ul>  
@@ -43,59 +40,13 @@
             <div class="ddatas">
                 <a href="./category.html">category</a>
                 <a href="./aboutUs.html">About Us</a>
+                <hr>
             </div>
             <a href="./login.html">Login</a>
             <a href="./register.html">Register</a>
         </div>
     </nav>
 
-    <!-- content -->
-    <!-- login -->
-
-    <div class="loginContainer">
-        <div class="loginBox">
-           <div class="loginBox2">
-                <div class="loginTitle">
-                    <P>Login</P>
-                </div>
-                <div class="inputanLogin">
-                    <div class="emailset">
-                        <label for="email">Email Address</label>
-                        <input type="text" id="email" name="email">
-                    </div>
-                    <div class="passwordset">
-                        <label for="password">Password</label>
-                        <input type="password" id="password" name="password">
-                    </div>
-                    <div class="rememberme">
-                        <input type="checkbox" id="kotak">
-                        <label for="kotak" id="rm">Remember Me</label>
-                    </div>
-                    <div class="loginbawah">
-                        <button class="loginbtn" onclick="doLogin()">Login</button>
-                        <a href="#" class="forgot">Forgot Your Password?</a>
-                    </div>
-                </div>
-           </div> 
-        </div>
-    </div>
-
-    <!-- Footer -->
-    <footer class="footer">
-        <div class="footercontent">
-            <div class="icons">
-                <a href="#"><img src="../assets/category/img/bxl-twitter.svg"></a>
-                <a href="#"><img src="..//assets/category/img/bxl-facebook.svg"></a>
-                <a href="#"><img src="../assets/category/img/bxl-instagram.svg"/></i></a>
-            </div>
-            <div class="text">
-                <p>Copyright @ 2021 Faiz Faqihudin . All Rights Reserved</p>
-            </div>
-        </div>
-    </footer>
+<script src="../script/navbar.js"></script>
 </body>
-<script>
-    // $('.homecontainer').hide();
-</script>
-<script src="../script/status.js"></script>
 </html>
