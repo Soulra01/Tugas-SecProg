20480f02989d4a2201f3e515dbc38e38205ca2a2
Soulra01 <raden.raharja@binus.ac.id>
2023-11-22 17:43:10 +0700

    cel

--- component/login.php
+++ component/login.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../assets/navbar.css">
+    <link rel="stylesheet" href="../assets/login/login.css">   
+    <link rel="stylesheet" href="../assets/footer.css">
+    
     <!-- <link rel="stylesheet" href="../assets/global.css"> -->
 </head>
 <body>
