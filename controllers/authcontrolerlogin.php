6aa51053af3b798fa70d29ebdb85d711f6ea78be
Soulra01 <raden.raharja@binus.ac.id>
2023-12-19 14:14:57 +0700

    x

--- controllers/authcontrolerlogin.php
+++ controllers/authcontrolerlogin.php
@@ -1,31 +1,36 @@
 <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    session_start();
     require "./connection.php";
     var_dump("Joshua Ganteng");
     if ($_SERVER['REQUEST_METHOD'] === "POST") {
         $userEmail = $_POST['email'];
         $userPassword = $_POST['password'];
-
-        $query = "SELECT * FROM  users WHERE email=? AND 'password'=?;";
+        
+        $query = "SELECT * FROM  users WHERE email=? AND password=?;";
         $stmt = $con->prepare($query);
         $stmt->bind_param("ss", $userEmail, $userPassword);
         $stmt->execute();
         $result = $stmt->get_result();
+        var_dump("Joshua Ganteng");
         $con->close();
 
-        if ($result->num_rows == 1) {
+        if ($result->num_rows === 1) {
             $row = $result->fetch_assoc();
 
             $_SESSION["success_message"] = "Login Success";
             $_SESSION['is_login'] = false;
-            $_SESSION['userName'] = $row['userName'];
-            $_SESSION['UserID'] = $row['UserID'];
+            $_SESSION['userName'] = $row['email'];
+            $_SESSION['UserID'] = $row['id'];
             header("Location: ../component/cek.php");
             echo "Login Success";
         }
         else {
             $_SESSION["error_message"] = "Login Failed";
-            echo "Login Failed";
             header("Location: ../component/login.php");
+            echo "Login Failed";
         }
     }
 ?>
